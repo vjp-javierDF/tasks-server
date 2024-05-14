c43c7ce0c620599a259ba0e3d6a5c2dd79914f86
javierFSoftinsa <163843790+javierFSoftinsa@users.noreply.github.com>
2024-05-14 14:31:47 +0100

    try to fix

--- src/main/java/dev/app/tasks/CorsConfig.java
+++ src/main/java/dev/app/tasks/CorsConfig.java
@@ -14,10 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://tasks-system.netlify.app/*")
+                        .allowedOriginPatterns("https://tasks-system.netlify.app/*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
 }
-
